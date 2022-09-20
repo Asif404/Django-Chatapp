9e157402cbf85b90fc2b6cbea4fda22e80630be0
Asif Muhammed <33574841+Asif404@users.noreply.github.com>
2022-09-20 21:35:27 +0530

    Update settings.py

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3o0oqy^(x1z*mywvx2i9z0s3283jk&@&5m9h80l76b+ii4*dcf'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
